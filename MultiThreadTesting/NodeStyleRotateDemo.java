fb02195ffcf83a182484694db4f92d6ec29889be
Genli01 <ligen01@gmail.com>
2017-06-12 22:35:10 -0700

    minor update

--- MultiThreadTesting/NodeStyleRotateDemo.java
+++ MultiThreadTesting/NodeStyleRotateDemo.java
@@ -5,7 +5,6 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 
